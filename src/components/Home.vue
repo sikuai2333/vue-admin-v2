4810b37eb4e2aaf8cef875cb271481f9c0f16f38
四块 <58365545+sikuai2333@users.noreply.github.com>
2023-03-15 17:23:57 +0800

    element-ui@2.15.8sass-loader@7node-sass@4reset.css

---
+++ src/components/Home.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="hello">
+    <h1>Hello</h1>
+    <el-button>hello</el-button>
+    <el-button type="primary">hello</el-button>
+    <el-button type="primary">hello</el-button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',  
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.hello{
+  background-color: yellow;
+  .el-button{
+    color: red;
+  }
+}
+</style>
