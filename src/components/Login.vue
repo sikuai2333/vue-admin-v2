2283587e2eb3f5e1b2e305c3ee212a25eff051d0
四块 <58365545+sikuai2333@users.noreply.github.com>
2023-03-16 00:13:52 +0800

    routers

---
+++ src/components/Login.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="hello">
+      <h1>Hello</h1>
+      <el-button>hello</el-button>
+      <el-button type="primary">hello</el-button>
+      <el-button type="primary">hello</el-button>
+      <i class="fa fa-users"></i>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'HelloWorld',  
+  }
+  </script>
+  
+  <!-- Add "scoped" attribute to limit CSS to this component only -->
+  <style scoped lang="scss">
+  .hello{
+    background-color: yellow;
+    .el-button{
+      color: red;
+    }
+  }
+  </style>
+  
